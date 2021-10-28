e977ee3a4c8c65eb28588d266735b416ccedbbea
Francis-Souza <francisleisouza@gmail.com>
2021-10-27 23:09:00 -0300

    create project

---
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot_mongodb/CursoDemoSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.joinsolutions.curso_demo_spring_boot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDemoSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDemoSpringBootMongodbApplication.class, args);
+	}
+
+}
