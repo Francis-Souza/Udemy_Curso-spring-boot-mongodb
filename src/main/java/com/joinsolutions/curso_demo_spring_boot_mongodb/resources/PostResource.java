287d3bc2e6b07adad72fb67e15d6a46dd0fb9dcc
Francis-Souza <francisleisouza@gmail.com>
2021-11-01 15:10:27 -0300

    Query methods

--- src/main/java/com/joinsolutions/curso_demo_spring_boot_mongodb/resources/PostResource.java
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot_mongodb/resources/PostResource.java
@@ -1,14 +1,19 @@
 package com.joinsolutions.curso_demo_spring_boot_mongodb.resources;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joinsolutions.curso_demo_spring_boot_mongodb.domain.Post;
 import com.joinsolutions.curso_demo_spring_boot_mongodb.domain.services.PostService;
+import com.joinsolutions.curso_demo_spring_boot_mongodb.resources.util.URL;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,6 +27,14 @@ public class PostResource {
 	public ResponseEntity<Post> findbyId(@PathVariable String id) {		
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	};
+
+
+	@GetMapping(value = "/posts/titlesearch")
+	public ResponseEntity<List<Post>> findbyTitle(@RequestParam(value="text", defaultValue = "") String text) {		
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	};
 
 	
