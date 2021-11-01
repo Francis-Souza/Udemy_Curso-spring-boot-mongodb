a8f6b6dfebf6dca541d055835cec63b0752b0dc5
Francis-Souza <francisleisouza@gmail.com>
2021-11-01 14:19:56 -0300

    Create PostDTO

--- src/main/java/com/joinsolutions/curso_demo_spring_boot_mongodb/config/Instantiation.java
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot_mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.joinsolutions.curso_demo_spring_boot_mongodb.domain.Post;
 import com.joinsolutions.curso_demo_spring_boot_mongodb.domain.User;
+import com.joinsolutions.curso_demo_spring_boot_mongodb.dto.AuthorDTO;
 import com.joinsolutions.curso_demo_spring_boot_mongodb.repositories.PostRepository;
 import com.joinsolutions.curso_demo_spring_boot_mongodb.repositories.UserRepository;
 
@@ -38,11 +39,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom Dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom Dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
